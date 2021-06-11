197b29e2a7e6cf0c9fd2d69f23fe5b1fb639ad79
phi <phi1618jp@gmail.com>
2021-06-12 02:17:47 +0900

    fix

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
 import './tags/**/*.pug';
+
+var admin = {
+  utils: {
+    '$get': (obj, key) => {
+      return key.split('.').reduce(function (t, v) {
+        return t && t[v];
+      }, obj);
+    },
+    '$set': (obj, key, value) => {
+      key.split('.').reduce(function (t, v, i, arr) {
+        if (i === (arr.length - 1)) {
+          t[v] = value;
+        }
+        else {
+          if (!t[v]) t[v] = {};
+          return t[v];
+        }
+      }, obj);
+    },
+  },
+};
+
+export default admin;

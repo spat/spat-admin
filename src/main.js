5808a151c79586974a8249adb20a73efc2f6b2fb
phi <phi1618jp@gmail.com>
2021-09-30 15:42:29 +0900

    move

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// import './tags/admin-field.pug';
+// import './tags/admin-item.pug';
+// import './tags/admin-page.pug';
+
+import './tags/**/*.pug';
+
+var admin = {
+  utils: {
+    '$get': (obj, key) => {
+      return key.split('.').reduce(function (t, v) {
+        return t && t[v];
+      }, obj);
+    },
+    '$set': (obj, key, value) => {
+      key.split('.').reduce(function (t, v, i, arr) {
+        if (i === (arr.length - 1)) {
+          t[v] = value;
+        }
+        else {
+          if (!t[v]) t[v] = {};
+          return t[v];
+        }
+      }, obj);
+    },
+  },
+};
+
+export default admin;
